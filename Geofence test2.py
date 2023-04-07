0be3975ddbf9e1f6fd603bca021b096f03cfb858
nxxvaa <exilednova09@gmail.com>
2023-04-07 15:43:41 +0200

    added file

--- Geofence test2.py
+++ Geofence test2.py
@@ -1,32 +1,55 @@
-lat = 30.0444
-lon = 31.2357
-alt = 0
+# lat = 30.0444
+# lon = 31.2357
+# alt = 0
 
-# Import pymavlink
-from pymavlink import mavutil
+from dronekit import connect, VehicleMode,mavutil
+from pymavlink.dialects.v20 import ardupilotmega as mavlink
+import time
 
-# Create a connection object
-connection = mavutil.mavlink_connection(device="tcp:127.0.0.1:5762")
+connection_string ='127.0.0.1:14550' #'tcp:127.0.0.1:5760'  #
+print('Connecting to vehicle on: %s' % connection_string)
+# The connect function will return an object of type Vehicle, which is the vehicle here
+vehicle = connect(connection_string, wait_ready=True)
+vehicle.commands.clear()
 
-# Wait for heartbeat
-connection.wait_heartbeat()
+waypoint1 = (37.4219999, -122.0840575, 0)
+waypoint2 = (38.4219999, -125.0839575, 0)
+waypoint3 = (39.4218999, -124.0839575, 0)
+waypoint4 = (36.4218999, -123.0840575, 0)
 
+cmds = vehicle.commands
+cmds.clear()
 
-
-connection.mav.MAV_CMD_NAV_FENCE_POLYGON_VERTEX_INCLUSION(3,0,0).send
-# msg = connection.mav.command_long_encode(
-#     0, # target system
-#     0, # sequence number
-#     mavutil.mavlink.MAV_CMD_NAV_FENCE_POLYGON_VERTEX_INCLUSION, # command
-#     3, # current
-#     0, # autocontinue
-#     0, # param1
+# vehicle.commands.add(
+#     mavlink.MAV_CMD_NAV_FENCE_POLYGON_VERTEX_INCLUSION,
+#     3,
 #     0,
-#     0, # param2
-#     lat, # x (latitude)
-#     lon, # param3
-#     0) # z (altitude)
+#     0,
+#     4,
+#     0,
+#     0,
+#     waypoint1[0],
+#     waypoint1[1],
+#     0
+# )
 
-# # Send the mission item to the vehicle
-# connection.mav.send(msg)
-print("done")
+# time.sleep(1)
+
+cmd = Command( 0,0,0,mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT,mavlink.MAV_CMD_NAV_FENCE_POLYGON_VERTEX_INCLUSION,3,0, 0, 0, waypoint1[0], waypoint1[1], 0)
+# time.sleep(1)
+
+# vehicle.commands.add(
+#     mavlink.MAV_CMD_NAV_FENCE_POINT_INCLUSION,
+#     0,
+#     0,
+#     0,
+#     0,
+#     0,
+#     0,
+#     waypoint3[0],
+#     waypoint3[1],
+#     waypoint3[2],
+#     0    
+# )
+cmds.add(cmd)
+cmds.upload()
